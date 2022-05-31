e38c56b766eb7782cdbab2e309c5e175f02e20af
Yesid Rodelo <yesiddeveloper@gmail.com>
2022-05-31 11:45:00 -0500

    fix some errors

--- nvim_fish.sh
+++ nvim_fish.sh
@@ -1,41 +1,54 @@
-# Fish shell
-sudo apt install fish
-
-#fisher (plugin to fish)
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-# Install nvm with fissher
-fisher install jorgebucaran/nvm.fish
-
-# Install nodejs
-nvm install v16.15.0
+#!/usr/bin/env fish
 
 #Oh my fish - Themes for fish shell
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install bobthefish
+
+#fisher (plugin to fish)
+echo -e "\nInstalling fisher"
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+
+# Install nvm with fisher
+echo -e "\nInstalling nvm"
+fisher install jorgebucaran/nvm.fish
+
+# Install nodejs
+echo -e "\nInstalling Nodejs"
+nvm install v16.15.0
+
+
+#!/usr/bin/env bash
 
 # Settings to fish shell
 cd ~/.config/fish
 echo 'set -g theme_color_scheme nord' > config.fish && echo 'set -Ua fish_user_path $Home/.local/share/nvm/v16.15.0/bin/node' >> config.fish
 
 # Vim plug
+echo -e "\nInstalling VimPlug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Ripgrep (rg)
+echo -e "\nInstalling ripgrep (rg)"
 sudo apt install ripgrep
 
 # Neovim
+echo -e "\nInstalling Neovim"
+
 cd ~
-curl https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
 sudo apt install ./nvim-linux64.deb
 rm -rf nvim-linux64.deb
 
-# Neovim Setup
-cd ~/.config
+
+echo -e "\nNeovim Setup"
+sudo apt install pynvim
+cd ~/.config/
 git clone https://github.com/Yesid-Rodelo/nvim.git
 
 # tmux and settings
+echo -e "\nInstalling tmux and configs"
 sudo apt install tmux
 cd ~
 touch .tmux.conf  
